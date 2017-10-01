7a14bf7520f34bf9b70b11088f3be59ee6eff58f
Amanda George <github email address>
2017-10-01 17:27:18 -0500

    add characters

---
+++ src/data/characters.js
@@ -0,0 +1,464 @@
+module.exports = [
+  {id: 1,
+  name: "Vincent Crabbe",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "green",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 2,
+  name: "Gregory Goyle",
+  img: "",
+  chars: {
+    gender: "male",
+    eyeColor: "brown",
+    hairColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 3,
+  name: "Blaise Zabini",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 4,
+  name: "Theodore Nott",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 5,
+  name: "Daphne Greengrass",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "blonde",
+    eyeColor: "green",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "slytherin"
+    }
+  },
+
+  {id: 6,
+  name: "Pansy Parkinson",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "slytherin"
+    }
+  },
+
+  {id: 7,
+  name: "Tracey Davis",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "blue",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "slytherin"
+    }
+  },
+
+  {id: 8,
+  name: "Millicent Bulstrode",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "black",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 9,
+  name: "Micheal Corner",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "black",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 10,
+  name: "Terry Boot",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 11,
+  name: "Anthony Goldstein",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "blonde",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 12,
+  name: "Kevin Entwhistle",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "green",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 13,
+  name: "Lisa Turpin",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "black",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 14,
+  name: "Morag Macdougal",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 15,
+  name: "Mandy Brocklehurst",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "blonde",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "ravenclaw"
+    }
+  },
+
+  {id: 16,
+  name: "Padma Patil",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "black",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "ravenclaw"
+    }
+  },
+  {id: 1,
+  name: "Vincent Crabbe",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "green",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "slytherin"
+    }
+  },
+
+  {id: 17,
+  name: "Dean Thomas",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "gryffindor"
+    }
+  },
+
+
+  {id: 18,
+  name: "Seamus Finnigan",
+  img: "",
+  chars: {
+    gender: "male",
+    eyeColor: "brown",
+    hairColor: "blue",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 19,
+  name: "Neville Longbottom",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 20,
+  name: "Harry Potter",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "green",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 21,
+  name: "Ron Weasley",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "red",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 22,
+  name: "Hermione Granger",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 23,
+  name: "Pavarti Patil",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "black",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 24,
+  name: "Lavender Brown",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "blonde",
+    eyeColor: "blue",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "gryffindor"
+    }
+  },
+
+  {id: 25,
+  name: "Ernie Macmillan",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "blonde",
+    eyeColor: "green",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 26,
+  name: "Justin Finch-Fetchley",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 27,
+  name: "Zacharias Smith",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "blonde",
+    eyeColor: "brown",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "short",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 28,
+  name: "Wayne Hopkins",
+  img: "",
+  chars: {
+    gender: "male",
+    hairColor: "brown",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "short",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 29,
+  name: "Megan Jones",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "blue",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 30,
+  name: "Leanne Moon",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "black",
+    eyeColor: "brown",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 31,
+  name: "Hannah Abbott",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "brown",
+    eyeColor: "brown",
+    hasScarf: false,
+    hasTie: true,
+    hairLength: "long",
+    house: "hufflepuff"
+    }
+  },
+
+  {id: 32,
+  name: "Susan Bones",
+  img: "",
+  chars: {
+    gender: "female",
+    hairColor: "red",
+    eyeColor: "blue",
+    hasScarf: true,
+    hasTie: false,
+    hairLength: "long",
+    house: "hufflepuff"
+    }
+  },
+
+]
