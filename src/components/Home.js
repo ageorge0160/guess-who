94b6881e82e2a93e77b994c6d3b0ed5910ee0224
Amanda George <github email address>
2017-10-02 08:18:45 -0500

    change home text and add link to game

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page </h1>
+      <h1>Welcome to Guess Who</h1>
+      <p>Our participants today come from the Hogwarts class of 1998</p>
+      <NavLink
+        to="/game"
+      >Begin Game</NavLink>
     </div>
   );
 };
