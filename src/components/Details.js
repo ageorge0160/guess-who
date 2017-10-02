9d0d696dd2ca56abacc79815d1919fa28ce5f15e
Amanda George <github email address>
2017-10-02 08:28:54 -0500

    add instructions and history to details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Details = () => {
   return (
     <div>
-      <h1>Details Page </h1>
+      <h1>How to play: </h1>
+        <p>Guess Who is a guessing game with images of characters from the Harry Potter book series. The object of the game is to determine which character has been chosen by the computer. This is done with yes or no questions pertaining to the characteristics of the characters shown. Characters that have been eliminated from the possibilities may be clicked on to hide them. Characterists that can be asked about include name, hair color, hair length, eye color, has scarf, has tie, house name, or gender.</p>
+      <h1>History</h1>
+        <p>This game is based off of the Guess Who? game from Milton Bradley, originally manufactured in 1979. It is a two-player character guessing game created by Ora and Theo Coster.</p>
     </div>
   );
 };
