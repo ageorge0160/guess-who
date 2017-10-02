aa0daca1a8b346e683843a8bc5cab20903ea9849
Amanda George <github email address>
2017-10-02 06:49:27 -0500

    create nav bar and reorganize files for correct route use

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import CharacterGrid from './components/CharacterGrid'
+import NavBar from '../src/components/NavBar';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
+import Home from '../src/components/Home';
+import Game from '../src/components/Game';
+import Details from '../src/components/Details';
+
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Guess Who</h1>
         </header>
-        <p className="App-intro">
-          <CharacterGrid />
-        </p>
+        <body>
+          <div>
+            <Router>
+              <div>
+                <NavBar />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/game" component={Game} />
+                <Route exact path="/details" component={Details} />
+              </div>
+            </Router>
+          </div>
+        </body>
       </div>
     );
   }
---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AnswerBox from './AnswerBox';
+import GuessForm from './GuessForm';
+import CharacterGrid from './CharacterGrid';
+
+const Game = () => {
+  return (
+    <div>
+      <h1>Game Page </h1>
+      <CharacterGrid />
+    </div>
+  );
+};
+
+export default Game;
