47ec577b1eb585f8ed008528aee99fbe97ea3189
Amanda George <github email address>
2017-10-01 19:06:05 -0500

    create files for characters board and add images to project

---
+++ src/components/Character.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class Character extends React.Component {
+  render() {
+    return (
+      <p> This is where each character will be shown </p>
+    )
+  }
+}
+export default Character
---
+++ src/components/CharacterGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Character from 'Character'
+
+export class CharacterGrid extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> This will be the body where the board goes in the component </p>
+        <Character />
+      </div>
+    )
+  }
+}
+export default CharacterGrid
